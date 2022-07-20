fa84b91f7ada08bd627ed1e78be27c0f7d46c182
Hafiz Bilal <iambilal@live.com>
2022-07-20 21:54:25 +0500

    Movies app complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
+import Movie from "./components/Movies";
+
+const FEATURED_API="https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
+const IMAGE_API="https://image.tmdb.org/t/p/w1280"
+const SEARCH_API="https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [movies,setMovies] = useState([]);
+  useEffect(()=>{
+    fetch(FEATURED_API)
+    .then((res)=>res.json())
+    .then((data)=>{
+      console.log(data);
+      setMovies(data.results);
+    },[]);
+  })
+  const handleonsubmit =(e)=>{
+    e.preventDefault();
+  }
+  return (<>
+ 
+    <header>
+      <form onSubmit={handleonsubmit}>
+        <input className="search" type="text" placeholder="Search..."></input>
+      </form>
+    </header>
+    <div className="movie-container">
+      {movies.length > 0 && movies.map((mov)=>(
+         <Movie key={mov.id} {...mov}/>
+      ))}
     </div>
+    </>
   );
 }
 
